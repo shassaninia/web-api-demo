572f0cc1656c9a9c5f5dc0c23237c08fb6842e91
shassaninia <shassaninia@ORLWS052>
2017-03-03 16:18:13 -0500

    Created EmployeesController with get method to return all employees

---
+++ WebAPIDemo/Controllers/EmployeesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using EmployeeDataAccess;
+
+namespace WebAPIDemo.Controllers
+{
+    public class EmployeesController : ApiController
+    {
+        public IEnumerable<Employee> Get()
+        {
+            using (var entities = new EmployeesDBEntities())
+            {
+                return entities.Employees.ToList();
+            }
+        }
+    }
+}
