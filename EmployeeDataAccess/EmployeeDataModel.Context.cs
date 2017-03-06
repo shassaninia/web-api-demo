75e3a71be9b77df9ee68fadb9a760822874c7929
shassaninia <shassaninia@ORLWS052>
2017-03-06 08:56:50 -0500

    Bug fixes

--- EmployeeDataAccess/EmployeeDataModel.Context.cs
+++ EmployeeDataAccess/EmployeeDataModel.Context.cs
@@ -25,7 +25,7 @@ namespace EmployeeDataAccess
             throw new UnintentionalCodeFirstException();
         }
     
-        public virtual DbSet<Employee> Employees { get; set; }
         public virtual DbSet<User> Users { get; set; }
+        public virtual DbSet<Employee> Employees { get; set; }
     }
 }
