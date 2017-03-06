5ab5369425830e7f0806559d6ee570610c1f9ce8
shassaninia <xai.soft80@gmail.com>
2017-03-06 07:27:57 -0500

    Insert Users script

---
+++ UsersInsert.sql
@@ -0,0 +1,9 @@
+﻿Create Table Users
+(
+Id int identity primary key,
+Username nvarchar(100),
+Password nvarchar(100)
+)
+
+Insert into Users values ('male','male')
+insert into Users values ('female','female')
