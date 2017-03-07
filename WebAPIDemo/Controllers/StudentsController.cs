5da1abd614b60fd9cb5690a20ba4928cfb5d89ba
shassaninia <shassaninia@ORLWS052>
2017-03-07 12:16:24 -0500

    To generate links, use route names. Set a NAME for the route using the Name property of the [Route] attribute and then use the NAME of the route to generate a link using Url.Link

--- WebAPIDemo/Controllers/StudentsController.cs
+++ WebAPIDemo/Controllers/StudentsController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
 using WebAPIDemo.Models;
@@ -15,49 +16,21 @@ namespace WebAPIDemo.Controllers
             new Student { Id = 3, Name = "John" },
         };
 
-        [Route("")]
-        public IEnumerable<Student> Get()
-        {
-            return students;
-        }
-
-        // = /api/students/1
-        [Route("{id:int:range(1,3)}")]
+        [Route("{id:int}", Name = "GetStudentById")]
         public Student Get(int id)
         {
             return students.FirstOrDefault(s => s.Id == id);
         }
 
-        [Route("{name:alpha}")]
-        public Student Get(string name)
+        public IHttpActionResult Post([FromBody] Student student)
         {
-            return students.FirstOrDefault(s => s.Name.ToLower() == name.ToLower());
+            students.Add(student);
+
+            var location = new Uri(Url.Link("GetStudentById", new { id = student.Id }));
+
+            return Created(location,student);
         }
 
-        // = /api/students/1/courses
-        [Route("{id}/courses")]
-        public IEnumerable<string> GetStudentCourses(int id)
-        {
-            if (id == 1)
-                return new List<string> { "C#", "ASP.NET", "SQL Server" };
-            else if (id == 2)
-                return new List<string> { "ASP.NET Web API", "C#", "SQL Server" };
-            else
-                return new List<string> { "Bootstrap", "jQuery", "AngularJs" };
-        }
 
-        //[Route("api/teachers")] - This will require you to put in /api/students/api/teachers because of the route prefix. To avoid this, use a Tilda symbol:
-        [Route("~/api/teachers")]
-        public IEnumerable<Teacher> GetTeachers()
-        {
-            List<Teacher> teachers = new List<Teacher>
-            {
-                new Teacher { Id = 1, Name = "Rob"},
-                new Teacher { Id = 2, Name = "Mike" },
-                new Teacher { Id = 3, Name = "Mary" }
-            };
-
-            return teachers;
-        }
     }
 }
